5702911af0a791f586c316372340510a0ae044b6
Matheus Mendonca <mmendonca@microsoft.com>
2022-08-02 09:14:25 -0300

    Fixes

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ def concat_lists(l1, l2):
     return l3
 
 def sum_lists(l1, l2):
+    assert type(l1) == list and type(l2) == list, "ERROR: l1 and l2 must be lists."
     assert len(l1) == len(l2), "ERROR: l1 and l2 must have the same size"
     l3 = []
     for i in range(len(l1)):
