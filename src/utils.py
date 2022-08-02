64b934e7dfe67ed6fe6ed344210a077c56e21b30
Matheus Mendonca <mmendonca@microsoft.com>
2022-08-02 09:05:08 -0300

    New src folder

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+def concat_lists(l1, l2):
+    l3 = l1 + l2
+    return l3
+
+def sum_lists(l1, l2):
+    assert len(l1) == len(l2), "ERROR: l1 and l2 must have the same size"
+    l3 = []
+    for i in range(len(l1)):
+        l3.append(l1[i] + l2[i])
+    return l3
